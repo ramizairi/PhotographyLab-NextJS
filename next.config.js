3d033168626dd2f6e7cd97feaf0add2741bd28cd
ramizairi <ramizeyri@gmail.com>
2025-02-22 09:12:38 +0100

    Disable Next.js image optimization for compatibility with CPanel

--- next.config.js
+++ next.config.js
@@ -1,32 +1,24 @@
 module.exports = {
   images: {
+    unoptimized: true, // Disable Next.js image optimization on CPanel
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        port: "",
         pathname: "/my-account/**",
       },
       {
         protocol: "https",
         hostname: "img.icons8.com",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "i.ibb.co",
-        port: "",
       },
       {
         protocol: "https",
         hostname: "i.pravatar.cc",
-        port: "",
-      },
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
       },
     ],
   },
