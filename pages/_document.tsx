7d94e27c040c3281b30fa1f68bdcd7beb3f79f37
ramizairi <ramizeyri@gmail.com>
2025-03-12 03:30:07 +0100

    commit documents

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 class MyDocument extends Document {
@@ -7,14 +7,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Frame Your Life." />
-          <meta property="og:site_name" content="Photography LAB - IHEC CARTHAGE" />
-          <meta property="og:description" content="Frame Your Life." />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:description" content="Frame Your Life." />
-
           {/* Google Analytics */}
           <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXX-X" />
           <Script
